da0dafe30ecadaecf8c5dc80f48ed621935aafe1
Matt Stencel <mattstencel@Matts-MacBook-Pro.local>
2024-05-06 01:36:09 -0600

    asd

--- DRDS/main2.py
+++ DRDS/main2.py
@@ -583,7 +583,7 @@ class MainApp(App):
     def build(self):
         Clock.schedule_interval(self.updatevariables, .1)
         Clock.schedule_interval(self.updateOBDdata, .01)
-        return Builder.load_file("main.kv")
+        return Builder.load_file("main2.kv")
 # ---------------------------------------------------------------------------------------------------------------------------------------------
     theme_cls = ThemeManager()
     version = StringProperty()
